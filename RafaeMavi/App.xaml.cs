70e3c0231fd8de57847fc91582f68b98b54e8942
rafaemavi <mavi.gouveia678@gmail.com>
2024-03-26 09:30:45 -0300

    26/03 começo

---
+++ RafaeMavi/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RafaeMavi;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ RafaeMavi/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RafaeMavi;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
