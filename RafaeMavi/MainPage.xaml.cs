1b277cc7419b4061db88e6b2b403304bc1c5fd5f
RafaeMavi <mavi.gouveia678@gmail.com>
2024-04-05 11:07:24 -0300

    05/04

--- RafaeMavi/MainPage.xaml.cs
+++ RafaeMavi/MainPage.xaml.cs
@@ -18,7 +18,12 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
         
 		atual = Lola;
-		imgPersonagem.Source=atual.GetArquivo();
+		AtualizaPersonagem();
+		var timer=Application.Current.Dispatcher.CreateTimer();
+		timer.Interval=TimeSpan.FromSeconds(5);
+		timer.Tick+=(s,e)=>
+		PassouTempo();
+		timer.Start();
 	}
 
 	void BotaoSetaFoiClicado(object sender, EventArgs args)
@@ -37,8 +42,42 @@ public partial class MainPage : ContentPage
 			}
 			imgPersonagem.Source=atual.GetArquivo();
 
-
 		}
+
+	void AtualizaPersonagem()
+	{
+		progressoFome.Progress=atual.GetFome();
+		progressoSede.Progress=atual.GetSede();
+		progressoAlegria.Progress=atual.GetAlegria();
+	}
+
+	void BotaoComidaFoiClicado(object sender, EventArgs args)	
+	   {
+		 atual.SetFome(atual.GetFome()+0.1);
+		 AtualizaPersonagem();
+	   }
+
+	void BotaoAguaFoiClicado(object sender, EventArgs args)	
+	   {
+		 atual.SetSede(atual.GetSede()+0.1);
+		 AtualizaPersonagem();
+	   }
+
+	void BotaoBrinquedoFoiClicado(object sender, EventArgs args)	
+	   {
+		 atual.SetAlegria(atual.GetAlegria()+0.1);
+		 AtualizaPersonagem();
+	   }
+
+	void PassouTempo()
+	{
+		atual.SetFome(atual.GetFome()-0.1);
+		atual.SetSede(atual.GetSede()-0.1);
+		atual.SetAlegria(atual.GetAlegria()-0.1);
+		AtualizaPersonagem();
+	}
+
+
 	
 }
 
